1d5253ee21b5cf223d10e910f4743985b4f93cdd
abhinavnigam99 <abhinav.nigam99@gmail.com>
2020-08-28 13:51:35 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgxSpinnerModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,104 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+import { isEmpty } from 'rxjs/operators';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  dataURL = 'https://api.spaceXdata.com/v3/launches?limit=100';
+  spaceData: any = [];
+  errorMessage = '';
+  year = [];
+
+
+  isYearClicked: any;
+  isLaunchClicked: boolean;
+  isLandingClicked: boolean;
+
+  constructor(private httpService: HttpService, private SpinnerService: NgxSpinnerService) { }
+
+  ngOnInit() {
+    for (let i = 2006; i < 2020; i++) {
+      this.year.push(i.toString());
+    }
+    this.generateUrl(this.dataURL);
+  }
+
+  getData(url) {
+    this.SpinnerService.show();
+    this.httpService.getData(url).subscribe(
+      (response: any) => {
+        for (const data of response) {
+          const singleData = {
+            flight_number: data.flight_number,
+            mission_name: data.mission_name,
+            mission_id: data.mission_id,
+            launch_year: data.launch_year,
+            launch_success: data.launch_success,
+            site_id: data.launch_site.site_id,
+            mission_patch: data.links.mission_patch,
+            mission_patch_small: data.links.mission_patch
+          };
+          this.spaceData.push(singleData);
+        }// Next callback
+        console.log(this.spaceData);
+        this.SpinnerService.hide();
+      },
+      (error) => {
+        this.SpinnerService.hide();
+        // Error callback
+        console.log(error);
+        this.errorMessage = error;
+      });
+  }
+
+  clickedYear(i) {
+    if (this.isYearClicked === this.year[i]) {
+      this.isYearClicked = undefined;
+    } else {
+      this.isYearClicked = this.year[i];
+    }
+    console.log(this.isYearClicked);
+    this.generateUrl(this.dataURL);
+  }
+
+  clickedLaunch(value) {
+    if (this.isLaunchClicked === value) {
+      this.isLaunchClicked = undefined;
+    } else {
+      this.isLaunchClicked = value;
+    }
+    this.generateUrl(this.dataURL);
+  }
+
+  clickedLanding(value) {
+    if (this.isLandingClicked === value) {
+      this.isLandingClicked = undefined;
+    } else {
+      this.isLandingClicked = value;
+    }
+    this.generateUrl(this.dataURL);
+  }
+
+  generateUrl(url) {
+    this.spaceData = [];
+    let filter = '';
+
+    if (this.isYearClicked !== undefined) {
+      filter += '&launch_year=' + this.isYearClicked;
+    }
+
+    if (this.isLaunchClicked !== undefined) {
+      filter += '&launch_success=' + this.isLaunchClicked;
+    }
+    if (this.isLandingClicked !== undefined) {
+      filter += '&land_success=' + this.isLandingClicked;
+    }
+    url += filter;
+    this.getData(url);
+  }
+}
